d751be15f57965f3c3722c06ae8056175e7f92f9
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-04-06 10:42:20 +0200

    starter boilerplate

--- program.py
+++ program.py
@@ -48,78 +48,7 @@ def create_votes():
 #################### FUNCTIONS ###################
 ##################################################
 
-############### CREATE ARRAY #####################
-
-def results_hash(votes):
-    """ Count votes per candidate and per mention
-
-    Returns a dict of candidate names containing vote arrays.
-    """
-    candidates_results = {
-        candidate: [0]*len(MENTIONS)
-        for candidate in CANDIDATES.keys()
-    }
-    for vote in votes:
-        for candidate, mention in vote.items():
-            candidates_results[candidate][mention] += 1
-    return candidates_results
-
-############### CALCULATE MEDIAN ##################
-
-def majoritary_mentions_hash(candidates_results):
-    r = {}
-    for candidate, candidate_result in candidates_results.items():
-        cumulated_votes = 0
-        for mention, vote_count in enumerate(candidate_result):
-            cumulated_votes += vote_count
-            if MEDIAN < cumulated_votes:
-                r[candidate] = {
-                    "mention": mention,
-                    "score": cumulated_votes
-                }
-                break
-    return r
-
-############### SORT CANDIDATES #####################
-
-def sort_candidates_by(mentions):
-    ## bubble sort here we go!
-    unsorted = [(key, (mention["mention"], mention["score"])) for key, mention in mentions.items()]
-    swapped = True
-    while swapped:
-        swapped = False
-        for j in range(0, len(unsorted) - 1):
-            ## but we need REVERSE bubble sort ;-)
-            # (note that here we compare tuples, which is pretty neat)
-            if unsorted[j + 1][1] > unsorted[j][1]:
-                unsorted[j+1], unsorted[j] = unsorted[j], unsorted[j+1]
-                swapped = True
-
-    return [
-        {
-            "name": candidate[0],
-            "mention": candidate[1][0],
-            "score": candidate[1][1],
-        }
-        for candidate in unsorted
-    ]
-
-############### FORMAT RESULTS #####################
-
-def print_results(results):
-    for i, result in enumerate(results):
-        name = CANDIDATES[result["name"]]
-        mention = MENTIONS[result["mention"]]
-        score = result["score"] * 100. / VOTES
-        if i == 0:
-            print("Gagnant: {} avec {:.2f}% de mentions {} ou inférieures".format(
-                name, score, mention
-            ))
-            continue
-        else:
-            print("- {} avec {:.2f}% de mentions {} ou inférieures".format(
-                name, score, mention
-            ))
+# Your code goes here
 
 
 ##################################################
@@ -128,10 +57,6 @@ def print_results(results):
 
 def main():
     votes = create_votes()
-    results = results_hash(votes)
-    majoritary_mentions = majoritary_mentions_hash(results)
-    sorted_candidates = sort_candidates_by(majoritary_mentions)
-    print_results(sorted_candidates)
 
 if __name__ == '__main__':
     main()
